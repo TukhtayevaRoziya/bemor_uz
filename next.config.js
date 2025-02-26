ffdb2a10672998d664eec3f64511eed726a2c702
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2025-02-26 21:41:27 +0500

    fixing errors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      _next_intl_trailing_slash: "false",
+    },
+    images: {
+      domains: ['example.com', 'another-example.com'],
+    },
+  };
+  
+  module.exports = nextConfig;
+
 module.exports = withNextIntl(nextConfig);
